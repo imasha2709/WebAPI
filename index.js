cbcdd4f48f9ae33f660cf527b3462ad3cf1f89bb
imasha <imashachandramali@gmail.com>
2026-07-16 12:54:40 +0530

    S2: hello-world app

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({
+        status: "ok",
+        session: "NB6007CEM S2"
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
